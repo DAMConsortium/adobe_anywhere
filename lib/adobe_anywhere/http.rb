2c10fb08422e62f321ef1ad293764cf92f7fc68d
john@damconsoritum.com <crantito>
2013-10-19 11:14:13 -0600

    Fixed logging.

--- lib/adobe_anywhere/http.rb
+++ lib/adobe_anywhere/http.rb
@@ -4,9 +4,31 @@ module AdobeAnywhere
 
   class HTTP < Sinatra::Base
 
+    enable :logging
+    disable :protection
+
     def self.init(args = { })
       args.each { |k,v| set(k, v) }
+
+      def logger; settings.logger end if settings.logger
     end # new
+
+    def format_response(response, args = { })
+      supported_types = [ 'application/json', 'application/xml', 'text/xml' ]
+      case request.preferred_type(supported_types)
+        when 'application/json'
+          content_type :json
+          _response = response.is_a?(Hash) || response.is_a?(Array) ? JSON.generate(response) : response
+        #when 'application/xml', 'text/xml'
+        #  content_type :xml
+        #  _response = XmlSimple.xml_out(response, { :root_name => 'response' })
+        else
+          content_type :json
+          _response = response.is_a?(Hash) || response.is_a?(Array) ? JSON.generate(response) : response
+      end
+      _response
+    end # output_response
+
 
     # Will try to convert a body to parameters and merge them into the params hash
     # Params will override the body parameters
@@ -35,33 +57,44 @@ module AdobeAnywhere
       _params = params.dup
       _params = merge_params_from_body(_params)
 
-      aa_args = { :logger => logger }
+      aa_args = { :logger => settings.logger }
       aa_args[:host_address] = search_hash!(_params, :host_address) || settings.anywhere_default_host_address
       aa_args[:port] = search_hash!(_params, :port) || settings.anywhere_default_host_port
       aa_args[:username] = search_hash!(_params, :username) || settings.anywhere_default_username
       aa_args[:password] = search_hash!(_params, :password) || settings.anywhere_default_password
+      #logger.debug { "AA Args: #{aa_args}" }
+      begin
+        aa = AdobeAnywhere::API::Utilities.new(aa_args)
+        aa.login
+      rescue => e
+        logger.warn { "Exception While Logging into Anywhere. #{e.message}\nBacktrace:\n#{e.backtrace}" }
+        return format_response({ :exception => { :message => e.message, :backtrace => e.backtrace } })
+      end
 
-      aa = AdobeAnywhere::API::Utilities.new(aa_args)
-      aa.login
+      method_name = search_hash!(_params, :method_name, :method, :command, :procedure)
+      return format_response({ :error => { :message => ':method_name is a required argument.' } }) unless method_name
 
-      command = search_hash!(_params, :procedure, :method, :command)
-      method_name = command.sub('-', '_').to_sym
+      method_name = method_name.sub('-', '_').to_sym
       method_arguments = search_hash!(_params, :arguments)
       method_arguments = JSON.parse(method_arguments) rescue method_arguments if method_arguments.is_a?(String)
-      logger.debug { "\nCommand: #{method_name}\nArguments: #{method_arguments}" }
+      logger.debug { "\nMethod Name: #{method_name}\nArguments: #{method_arguments}" }
 
       send_args = [ method_name ]
       send_args << method_arguments if method_arguments
       @safe_methods ||= aa.methods - Object.new.methods
       if @safe_methods.include?(method_name)
-        _response = aa.send(*send_args)
+        begin
+          _response = aa.send(*send_args)
+        rescue => e
+          logger.warn { "Error executing api command. #{e.message}\nBacktrace:\n#{e.backtrace}" }
+          _response = { :exception => { :message => e.message, :backtrace => e.backtrace } }
+        end
       else
         _response = { :error => "#{method_name} is not a valid method name." }
       end
       logger.debug { "Response: #{_response}" }
-      _response
+      format_response(_response)
     end
-
 
   end # HTTP
 
