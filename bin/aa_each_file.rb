f566cd1e648c9dce24e67b9387455c434a258816
John Whitson <john.whitson@gmail.com>
2014-05-23 16:22:26 -0600

    Fixed issue with production name option Added production-create-if-not-exists option Added asset-create-if-not-exists option

--- bin/aa_each_file.rb
+++ bin/aa_each_file.rb
@@ -12,13 +12,18 @@ require 'shellwords'
 def options; @options end
 
 options[:executable_path] = '/Library/Scripts/ubiquity/adobe_anywhere/bin/aa'
+options[:production_create_if_not_exists] = true
+options[:asset_create_if_not_exists] = true
 options[:recursive] = true
 op = OptionParser.new
+op.banner = "#{File.basename($0)} [options] file_path1 [file_path2] [file_path3] [...]"
 op.on('--aa-executable-path PATH', 'The path to the Adobe Anywhere command line executable.', "\tdefault: #{options[:executable_path]}")
 op.on('--mount-point-name NAME', 'The name of the media_paths mountpoint.', 'i.e: MEDIA') { |v| options[:mount_point_name] = v }
 op.on('--path-substitutions JSON', 'Any path substitutions to perform on the file paths.', %q('{"/Volumes":"", "/root":""}')) { |v| options[:path_substitutions] = JSON.parse(v) }
-op.on('--production-name NAME', 'A production name to add the assets to.') { options[:production_name] = v }
+op.on('--production-name NAME', 'A production name to add the assets to.') { |v| options[:production_name] = v }
 op.on('--[no-]recursive', 'Determines if folders will be recursed into.', "\tdefault: #{options[:recursive] ? 'true' : 'false'}") { |v| options[:recursive] = v }
+op.on('--[no-[production-create-if-not-exists', 'Determine if the production will be created if it does not exist.', "\tdefault: #{options[:production_create_if_not_exists] ? 'true' : 'false'}") { |v| options[:production_create_if_not_exists] = v }
+op.on('--[no-]asset-create-if-not-exists', 'Will check to see if the asset exists before attempting to add it to the production.', "\tdefault: #{options[:asset_create_if_not_exists] ? 'true' : 'false'}") { |v| options[:asset_create_if_not_exists] = v }
 op.on('--help', 'Displays this message') { puts op; exit }
 
 original_arguments = ARGV.dup
@@ -32,21 +37,25 @@ folder_paths = paths
 abort('A folder path must be specified.') if folder_paths.empty?
 
 @executable_path = options[:executable_path]
-@workflow_name = options[:workflow_name]
+@asset_create_if_not_exists = options[:asset_create_if_not_exists]
 @path_substitutions = options[:path_substitutions] || { }
 @mount_point_name = options[:mount_point_name]
 @production_name = options[:production_name]
+@production_create_if_not_exists = options[:production_create_if_not_exists]
 @recursive = options[:recursive]
 
 @logger = Logger.new(STDERR)
 def logger; @logger end
 logger.level = Logger::DEBUG
 
+def asset_create_if_not_exists; @asset_create_if_not_exists end
 def mount_point_name; @mount_point_name end
 def path_substitutions; @path_substitutions end
 def executable_path; @executable_path end
 def production_name; @production_name end
+def production_create_if_not_exists; @production_create_if_not_exists end
 def file_path; @file_path end
+
 def recursive; @recursive end
 
 def execute(command_line)
@@ -66,7 +75,7 @@ def aa_execute(*args)
   _file_path = File.join(mount_point_name, _file_path) if mount_point_name
 
   #command_line = %(#{executable_path} --method-name production_asset_add --method-arguments '{ "production_name": "#{production_name}", "media_paths": "eamedia://#{_file_path}", "create_asset_if_not_exists":true }')
-  method_arguments_json = %({ "production_name": "#{production_name}", "media_paths": "eamedia://#{_file_path}", "create_asset_if_not_exists":true })
+  method_arguments_json = %({ "production_name": "#{production_name}", "media_paths": "eamedia://#{_file_path}", "create_asset_if_not_exists":#{asset_create_if_not_exists ? 'true' : 'false'}, "production_create_if_not_exists":#{production_create_if_not_exists ? 'true' : 'false'} })
   command_array = [ executable_path, '--method-name', 'production_asset_add', '--method-arguments', method_arguments_json ]
   command_line = command_array.shelljoin
   execute(command_line)
